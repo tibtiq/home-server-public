f67869a09b40d2f9fc1639429413ae7f8913da4c
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-05-02 15:47:06 +0000

    Update from tibtiq/home-server
    Update from https://github.com/tibtiq/home-server/commit/e3806ea465b67f960c5d33697bc4249f3cee964c

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -16,8 +16,22 @@ if useradd -m -s /bin/bash "$username" 2>&1 | grep -q "already exists"; then
     exit 1
 fi
 
-# add them to the sudo group
-usermod -aG sudo "$username"
+# setup group that can sudo with no password
+groupadd sudo_no_password
+
+# update group to allow passwordless sudo
+LINE='%sudo_no_password ALL=(ALL) NOPASSWD: ALL'
+FILE='/etc/sudoers'
+if sudo grep -Fxq "$LINE" "$FILE"; then
+    echo "Passwordless sudo setting for group sudo_no_password already exists in $FILE"
+fi
+echo "$LINE" | sudo EDITOR='tee -a' visudo
+
+# add them to the group
+usermod -aG sudo_no_password "$username"
+
+# install sudo
+apt install sudo
 
 # set a password (if you want, not required if using ssh keys to log in)
 # passwd "$username"
