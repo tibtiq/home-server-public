d80da9da36fa8f8032fc115edc30f41e18118399
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-05-21 19:25:00 -0700

    Update setup.sh

--- nixos/lxc_public/lxc_public/scripts/setup.sh
+++ nixos/lxc_public/lxc_public/scripts/setup.sh
@@ -5,7 +5,7 @@ NIX_CONFIG_PATH='/etc/nixos/configuration.nix'
 curl \
   --show-error \
   --fail \
-  https://raw.githubusercontent.com/tibtiq/nix-config-public/refs/heads/main/proxmox_lxc_config.nix \
+  https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/nixos/lxc_public/lxc_public/proxmox_lxc_config.nix \
   >${NIX_CONFIG_PATH}
 
 sudo nix-channel --update && sudo nixos-rebuild switch --upgrade
