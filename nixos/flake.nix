40dfa4dd0b8fa47991b1837d3b4654a1f0d21e45
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-06-17 22:16:32 +0000

    add sample consolidated flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,21 +2,56 @@
   description = "My NixOS Configuration";
 
   inputs = {
-    # Nixpkgs and flake-utils
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations.proxmox_lxc_fresh = nixpkgs.lib.nixosSystem {
-        inherit system;
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        # For devShells or shared packages if needed
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.git pkgs.nixpkgs-fmt ];
+        };
+      }) // {
+        nixosConfigurations = {
+          laptop = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./hosts/laptop.nix
+            ];
+          };
 
-        modules = [
-          ./proxmox_lxc.nix
-        ];
+          server = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./hosts/server.nix
+            ];
+          };
+        };
+
+        homeConfigurations = {
+          "tibtiq@laptop" = home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs { system = "x86_64-linux"; };
+            modules = [
+              ./home/common.nix
+              ./home/tibtiq@laptop.nix
+            ];
+          };
+        };
       };
-    };
 }
