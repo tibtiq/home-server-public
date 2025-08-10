126de18c5c198719b5a1e4686aca003c890beb88
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-08-10 12:01:01 -0700

    Update download_github_ssh_keys.sh

--- scripts/download_github_ssh_keys.sh
+++ scripts/download_github_ssh_keys.sh
@@ -1,1 +1,8 @@
+# The following command will download and immediately run the bash script.
+# bash <(wget -qO- https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/download_github_ssh_keys.sh)
+
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 mkdir -p ~/.ssh && curl "https://github.com/tibtiq.keys" > ~/.ssh/authorized_keys
