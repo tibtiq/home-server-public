d2ecd6d5fa110c7ead00c9434207c9159a59fb51
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-09-21 17:36:56 -0700

    update script so it runs as root

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -24,10 +24,10 @@ groupadd sudo_no_password
 # update group to allow passwordless sudo
 LINE='%sudo_no_password ALL=(ALL) NOPASSWD: ALL'
 FILE='/etc/sudoers'
-if sudo grep -Fxq "$LINE" "$FILE"; then
+if grep -Fxq "$LINE" "$FILE"; then
     echo "Passwordless sudo setting for group sudo_no_password already exists in $FILE"
 fi
-echo "$LINE" | sudo EDITOR='tee -a' visudo
+echo "$LINE" | EDITOR='tee -a' visudo
 
 # add them to the group
 usermod -aG sudo_no_password "$username"
