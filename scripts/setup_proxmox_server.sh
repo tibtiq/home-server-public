0d6fc189d22d5d73f1963c1b8b912fee1a26e456
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-09-21 17:27:43 -0700

    add main script for setting up proxmox server

---
+++ scripts/setup_proxmox_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# The following command will download and immediately run the bash script.
+# curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/setup_proxmox_server.sh) | bash -s -- USERNAME
+
+set -euo pipefail
+IFS=$'\n\t'
+
+curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/create_user.sh | bash -s -- "$@"
