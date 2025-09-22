88eebaf7e58dc89f35dd78eb435ea62ed63a6237
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-09-21 17:35:45 -0700

    typo

--- scripts/setup_proxmox_server.sh
+++ scripts/setup_proxmox_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The following command will download and immediately run the bash script.
-# curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/setup_proxmox_server.sh) | bash -s -- USERNAME
+# curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/setup_proxmox_server.sh | bash -s -- USERNAME
 
 set -euo pipefail
 IFS=$'\n\t'
