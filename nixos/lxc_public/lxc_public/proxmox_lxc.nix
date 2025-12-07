c45a9cfbcd2ed99f200be9b6f00a36ef0254f842
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-12-07 08:54:32 +0000

    remove unnecessary setting

--- nixos/lxc_public/lxc_public/proxmox_lxc.nix
+++ nixos/lxc_public/lxc_public/proxmox_lxc.nix
@@ -29,21 +29,6 @@ in {
     tmux  # tui for git
   ];
 
-  i18n = {
-    defaultLocale = defaultLocale;
-    extraLocaleSettings = {
-      LC_ADDRESS = defaultLocale;
-      LC_IDENTIFICATION = defaultLocale;
-      LC_MEASUREMENT = defaultLocale;
-      LC_MONETARY = defaultLocale;
-      LC_NAME = defaultLocale;
-      LC_NUMERIC = defaultLocale;
-      LC_PAPER = defaultLocale;
-      LC_TELEPHONE = defaultLocale;
-      LC_TIME = defaultLocale;
-    };
-  };
-
   users = {
     mutableUsers = false;
     users."${user}" = {
