4b04c8beaefc58c55670004ffff11dd8d3c3b7dd
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-12-08 01:02:35 +0000

    add command to run directly

--- nixos/lxc_public/lxc_public/scripts/setup.sh
+++ nixos/lxc_public/lxc_public/scripts/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# The following command will download and immediately run the bash script.
+# curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/nixos/lxc_public/lxc_public/scripts/setup.sh | bash
+
 NIX_CONFIG_PATH='/etc/nixos/configuration.nix'
 
 # download config
