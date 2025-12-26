3f538f90fbf9c96b9ae10dab27fb87ca363df25a
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-12-26 05:26:40 +0000

    fix permapath

--- scripts/create_nixos_lxc.sh
+++ scripts/create_nixos_lxc.sh
@@ -22,7 +22,7 @@ CONTAINER_DISK_SIZE_IN_GB='64'
 wget https://github.com/tibtiq.keys -O ~/.ssh/github_keys.pub
 
 # download NixOS image
-curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/create_nixos_lxc.sh | bash
+curl -fsSL https://raw.githubusercontent.com/tibtiq/home-server-public/refs/heads/main/scripts/download_nixos_image.sh | bash
 
 # create LXC
 pct create "$(pvesh get /cluster/nextid)" \
